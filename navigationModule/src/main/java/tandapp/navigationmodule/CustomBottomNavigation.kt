72dbd642a4bac210e255eeadcd2a13da8a831279
yerakulmanov <200103127@stu.sdu.edu.kz>
2024-03-06 15:30:03 +0500

    Implemented bottom navigation bar

--- navigationModule/src/main/java/tandapp/navigationmodule/CustomBottomNavigation.kt
+++ navigationModule/src/main/java/tandapp/navigationmodule/CustomBottomNavigation.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -18,6 +20,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -33,8 +36,13 @@ import tandapp.utillibrary.click
 import tandapp.utillibrary.values.Gray
 import tandapp.utillibrary.values.Purple
 import tandapp.utillibrary.values.Silver
+import tandapp.utillibrary.values.cornerRadius16
 import tandapp.utillibrary.values.fontSize11
 import tandapp.utillibrary.values.lineHeight13
+import tandapp.utillibrary.values.spacing12
+import tandapp.utillibrary.values.spacing16
+import tandapp.utillibrary.values.spacing2
+import tandapp.utillibrary.values.spacing20
 import tandapp.utillibrary.values.spacing4
 import tandapp.utillibrary.values.spacing8
 
@@ -43,22 +51,22 @@ fun CustomBottomNavigation(
     navController: NavController,
     scope: CoroutineScope,
     onNavClick: (itemDest: String) -> Unit = {}
-){
+) {
     val items = remember {
-            listOf(
-                MainDestinations.HOME,
-                MainDestinations.CATALOG,
-                MainDestinations.CHAT,
-                MainDestinations.BACKET,
-                MainDestinations.PROFILE
-            )
+        listOf(
+            MainDestinations.HOME,
+            MainDestinations.CATALOG,
+            MainDestinations.CHAT,
+            MainDestinations.BACKET,
+            MainDestinations.PROFILE
+        )
     }
 
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
 
     Column {
-        Divider(color = Gray)
+        Divider(color = Silver)
 
         Row(
             modifier = Modifier
@@ -94,37 +102,62 @@ fun CustomBottomNavigation(
 @Composable
 private fun BottomNavigationItem(
     icon: Int,
-    totalUnreadMessages: Int? = 0,
     name: String,
     isChat: Boolean = false,
     selected: Boolean,
     modifier: Modifier,
     onClick: () -> Unit
 ) {
-    Column(
-        modifier = modifier
-            .click(onClick = onClick)
-            .padding(vertical = spacing8),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(spacing8)
-    ) {
-        Box {
+    if (isChat) {
+        Column(
+            modifier = modifier
+                .offset(y = (-15).dp)
+                .click(onClick = onClick),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(100))
+                    .background(Purple)
+                    .padding(horizontal = spacing20, vertical = spacing12),
+            ) {
+                Icon(
+                    painterResource(id = icon),
+                    contentDescription = null,
+                    tint = Color.White,
+                    modifier = Modifier.size(24.dp),
+                )
+                Spacer(modifier = Modifier.height(spacing2))
+                Text(
+                    text = name,
+                    fontSize = fontSize11,
+                    fontWeight = FontWeight.Normal,
+                    color = Color.White,
+                )
+            }
+        }
+    } else {
+        Column(
+            modifier = modifier
+                .click(onClick = onClick)
+                .padding(vertical = spacing8),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(spacing8)
+        ) {
             Icon(
                 painterResource(id = icon),
                 contentDescription = null,
                 tint = if (selected) Purple else Gray.copy(0.4f),
                 modifier = Modifier.size(28.dp)
             )
-        }
 
-        Text(
-            text = name,
-            fontSize = fontSize11,
-            fontWeight = if (selected) FontWeight.Medium else FontWeight.Normal,
+            Text(
+                text = name,
+                fontSize = fontSize11,
+                fontWeight = if (selected) FontWeight.Medium else FontWeight.Normal,
 //            style = if (selected) medium else regular,
-            maxLines = 1,
-            color = if (selected) Purple else Gray,
-            overflow = TextOverflow.Ellipsis
-        )
+                color = if (selected) Purple else Gray,
+            )
+        }
     }
 }
