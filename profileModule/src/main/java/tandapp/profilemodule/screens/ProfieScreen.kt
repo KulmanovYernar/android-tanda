c08fbade452cec1eccc4610314df9d0dfc13fbed
yerakulmanov <200103127@stu.sdu.edu.kz>
2024-03-27 18:16:09 +0500

    Implemented ProfileScreen

--- profileModule/src/main/java/tandapp/profilemodule/screens/ProfieScreen.kt
+++ profileModule/src/main/java/tandapp/profilemodule/screens/ProfieScreen.kt
@@ -1,10 +1,14 @@
 package tandapp.profilemodule.screens
 
 import androidx.activity.compose.BackHandler
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,6 +16,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -19,28 +25,32 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import kotlinx.coroutines.launch
-import tandapp.icons.R
 import tandapp.navigationmodule.CustomBottomNavigation
-import tandapp.utillibrary.buttons.CustomButton
-import tandapp.utillibrary.buttons.CustomButtonText
-import tandapp.utillibrary.toolbars.DefaultHomeToolbar
-import tandapp.utillibrary.values.Gray
+import tandapp.utillibrary.click
+import tandapp.utillibrary.toolbars.DefaultToolbarWithEditButton
+import tandapp.utillibrary.ui_components.BoxImage
+import tandapp.utillibrary.values.Silver3
 import tandapp.utillibrary.values.Silver4
-import tandapp.utillibrary.values.cornerRadius8
-import tandapp.utillibrary.values.fontSize11
+import tandapp.utillibrary.values.cornerRadius10
+import tandapp.utillibrary.values.fontSize13
 import tandapp.utillibrary.values.fontSize16
-import tandapp.utillibrary.values.lineHeight18
-import tandapp.utillibrary.values.lineHeight22
+import tandapp.utillibrary.values.lineHeight20
+import tandapp.utillibrary.values.lineHeight24
+import tandapp.utillibrary.values.spacing10
 import tandapp.utillibrary.values.spacing16
 import tandapp.utillibrary.values.spacing2
+import tandapp.utillibrary.values.spacing20
 import tandapp.utillibrary.values.spacing24
+import tandapp.utillibrary.values.spacing32
 import tandapp.utillibrary.values.spacing4
 import tandapp.utillibrary.values.spacing8
 
@@ -72,67 +82,84 @@ fun ProfileScreen(
                         .fillMaxWidth(),
                     contentAlignment = Alignment.Center
                 ) {
-                    DefaultHomeToolbar(title = "Профиль",
+                    DefaultToolbarWithEditButton(
+                        title = "Профиль",
                         titleColor = Color.Black,
-                        onCityClick = {}
+                        onEditClick = {}
                     )
                 }
             }
         },
         content = {
-            it.calculateTopPadding()
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(top = spacing8, bottom = spacing8)
+                    .padding(
+                        top = spacing8,
+                        bottom = it.calculateBottomPadding(),
+                        start = spacing16,
+                        end = spacing16
+                    )
             ) {
-                    Column(
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(id = tandapp.icons.R.drawable.circle_photo),
+                        contentDescription = null,
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = spacing24),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Icon(
-                            imageVector = ImageVector.vectorResource(R.drawable.ic_backet),
-                            contentDescription = null,
-                            tint = Gray.copy(0.8f),
-                            modifier = Modifier.size(40.dp)
-                        )
-                        Spacer(modifier = Modifier.height(spacing2))
+                            .size(50.dp)
+                            .clip(CircleShape)
+                            .click {
+
+                            }
+                    )
+                    Spacer(modifier = Modifier.width(spacing4))
+                    Column(verticalArrangement = Arrangement.spacedBy(spacing2)) {
                         Text(
-                            text = "Корзина пуста",
-                            color = Color.Black,
+                            text = "Murat Altynay",
                             fontSize = fontSize16,
-                            lineHeight = lineHeight22,
+                            lineHeight = lineHeight24,
+                            color = Color.Black,
                             fontWeight = FontWeight.Medium
                         )
-                        Spacer(modifier = Modifier.height(spacing4))
+
                         Text(
-                            text = "Воспользуйтесь чатом, чтобы найти все, что нужно.",
-                            color = Silver4,
-                            fontSize = fontSize11,
-                            lineHeight = lineHeight18,
+                            text = "murat@moinak.kz",
+                            fontSize = fontSize13,
+                            lineHeight = lineHeight20,
+                            color = Color.Blue,
                             fontWeight = FontWeight.Normal
                         )
-                        Spacer(modifier = Modifier.height(spacing8))
-                        CustomButton(
-                            modifier = Modifier
-                                .width(118.dp)
-                                .height(40.dp),
-                            cornerRadius = cornerRadius8,
-                            content = {
-                                CustomButtonText(
-                                    text = "На главную",
-                                    fontSize = fontSize16,
-                                    lineHeight = lineHeight18
-                                )
-                            }) {
-                            onBack()
-                        }
-                        Spacer(modifier = Modifier.height(spacing24))
                     }
                 }
+                Spacer(modifier = Modifier.height(spacing24))
+                Column(verticalArrangement = Arrangement.spacedBy(spacing20)) {
+                    ChangeProfilePhoto(onClick = {})
+                    DefaultRowItem(
+                        icon = tandapp.icons.R.drawable.language,
+                        title = "Язык",
+                        hint = "Русский",
+                        onClick = {}
+                    )
+                    DefaultRowItem(
+                        icon = tandapp.icons.R.drawable.ic_favorite,
+                        title = "Избранные",
+                        hint = "24",
+                        onClick = {}
+                    )
+                    DefaultRowItem(
+                        icon = tandapp.icons.R.drawable.ic_faq,
+                        title = "Tanda App FAQ",
+                        onClick = {}
+                    )
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                LogOut(onLogOutClick = {})
+                Spacer(modifier = Modifier.height(spacing32))
+            }
         },
         bottomBar = {
             CustomBottomNavigation(
@@ -147,4 +174,116 @@ fun ProfileScreen(
         }
     )
 
+}
+
+@Composable
+fun ChangeProfilePhoto(onClick: () -> Unit){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                0.5.dp, color = Silver3, shape = RoundedCornerShape(
+                    cornerRadius10
+                )
+            )
+            .padding(horizontal = spacing16, vertical = spacing10)
+            .click {
+                onClick()
+            },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = ImageVector.vectorResource(tandapp.icons.R.drawable.ic_camera),
+            contentDescription = null,
+            modifier = Modifier.size(24.dp),
+            tint = Color.Blue
+        )
+        Spacer(modifier = Modifier.width(spacing16))
+        Text(
+            text = "Изменить фото",
+            color = Color.Blue,
+            fontSize = fontSize13,
+            lineHeight = lineHeight20,
+            fontWeight = FontWeight.Normal
+        )
+    }
+}
+@Composable
+fun DefaultRowItem(
+    @DrawableRes icon: Int,
+    title: String,
+    hint: String = "",
+    onClick: () -> Unit = {}
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                0.5.dp, color = Silver3, shape = RoundedCornerShape(
+                    cornerRadius10
+                )
+            )
+            .padding(horizontal = spacing16, vertical = spacing10)
+            .click {
+                onClick()
+            },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        BoxImage(
+            imageRes = icon,
+            boxSize = 24.dp,
+            maxWidth = 16.dp,
+            maxHeight = 16.dp,
+            cornerRadius = 4.dp
+        )
+        Spacer(modifier = Modifier.width(spacing16))
+        Text(
+            text = title,
+            color = Color.Black,
+            fontSize = fontSize13,
+            lineHeight = lineHeight20,
+            fontWeight = FontWeight.Normal
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        if (hint.isNotBlank()) {
+            Text(
+                text = hint,
+                color = Silver4,
+                fontSize = fontSize13,
+                lineHeight = lineHeight20,
+                fontWeight = FontWeight.Normal
+            )
+            Spacer(modifier = Modifier.width(spacing2))
+        }
+        Icon(
+            imageVector = ImageVector.vectorResource(tandapp.icons.R.drawable.ic_arrow_right),
+            contentDescription = null,
+            modifier = Modifier.size(24.dp),
+            tint = Silver4
+        )
+    }
+}
+
+@Composable
+fun LogOut(onLogOutClick:() -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(
+                0.5.dp, color = Silver3, shape = RoundedCornerShape(
+                    cornerRadius10
+                )
+            )
+            .padding(horizontal = spacing16, vertical = spacing10),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Log Out",
+            color = Color.Red,
+            fontSize = fontSize13,
+            lineHeight = lineHeight20,
+            fontWeight = FontWeight.Normal
+        )
+    }
 }
--- utillibrary/src/main/java/tandapp/utillibrary/toolbars/DefaultToolbar.kt
+++ utillibrary/src/main/java/tandapp/utillibrary/toolbars/DefaultToolbar.kt
@@ -1,12 +1,13 @@
 package tandapp.utillibrary.toolbars
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.windowInsetsTopHeight
@@ -19,13 +20,12 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import tandapp.utillibrary.buttons.BackButton
 import tandapp.utillibrary.click
-import tandapp.utillibrary.values.Purple
 import tandapp.utillibrary.values.fontSize16
-import tandapp.utillibrary.values.fontSize18
 import tandapp.utillibrary.values.fontSize22
+import tandapp.utillibrary.values.lineHeight22
 import tandapp.utillibrary.values.spacing16
-import tandapp.utillibrary.values.spacing24
 import tandapp.utillibrary.values.spacing4
+import tandapp.utillibrary.values.spacing8
 
 @Composable
 fun DefaultToolbar(
@@ -58,6 +58,46 @@ fun DefaultToolbar(
                     color = Color.Black,
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DefaultToolbarWithEditButton(
+    title: String = "",
+    titleColor:Color = Color.Black,
+    onEditClick: () -> Unit
+) {
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .background(color = Color.White)
+            .padding(vertical = spacing8)
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = title,
+                    fontSize = fontSize22,
+                    fontWeight = FontWeight.Bold,
+                    color = titleColor,
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Text(
+                    text = "Изменить",
+                    fontSize = fontSize16,
+                    lineHeight = lineHeight22,
+                    color = Color.Blue,
+                    modifier = Modifier.click {
+                        onEditClick()
+                    }
                 )
             }
         }
