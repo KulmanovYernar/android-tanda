3ceb8f86ac6079842116fad5bdecc28ac80cde16
yerakulmanov <200103127@stu.sdu.edu.kz>
2024-05-27 12:31:58 +0500

    Created PaymentScreen

---
+++ backetModule/src/main/java/tandapp/backetmodule/screens/PaymentScreen.kt
@@ -0,0 +1,12 @@
+package tandapp.backetmodule.screens
+
+import androidx.compose.runtime.Composable
+import org.koin.androidx.compose.getViewModel
+import tandapp.backetmodule.viewmodels.PaymentViewModel
+
+@Composable
+fun PaymentScreen(
+    viewModel: PaymentViewModel = getViewModel()
+){
+
+}
---
+++ backetModule/src/main/java/tandapp/backetmodule/viewmodels/PaymentViewModel.kt
@@ -0,0 +1,7 @@
+package tandapp.backetmodule.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class PaymentViewModel(): ViewModel() {
+
+}
